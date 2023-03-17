88eef38e4d73c70759048ce79163994e870e8fd0
Siddhant Patel <siddhantpatel445@gmail.com>
2023-03-17 16:38:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android_learning_app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.android_learning_app
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputBinding
+import com.example.android_learning_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.
+        binding.btnWebView.setOnClickListener {
+            val intent = Intent(this , show_view::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/android_learning_app/show_view.kt
@@ -0,0 +1,51 @@
+package com.example.android_learning_app
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.webkit.WebResourceRequest
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.annotation.RequiresApi
+import com.example.android_learning_app.databinding.ActivityShowViewBinding
+
+
+@Suppress("DEPRECATION")
+open class show_view : AppCompatActivity() {
+
+    private lateinit var binding: ActivityShowViewBinding
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityShowViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.show.webViewClient = show_web_view()
+
+        binding.show.loadUrl("https://music.youtube.com/")
+        binding.show.settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        binding.show.settings.javaScriptEnabled = true
+        binding.show.webViewRenderProcess
+        getSupportActionBar()?.hide()
+    }
+    inner class show_web_view : WebViewClient(){
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            binding.progressbar.visibility = View.GONE
+        }
+        override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+    }
+
+    override fun onBackPressed() {
+        if(binding.show.canGoBack()){
+            binding.show.goBack()
+        }else {
+            super.onBackPressed()
+        }
+    }
+}
